39c704af65a9f4f7696f7aa45131fa1c5c9ab764
IchBinEineKartoffel <stephen.lonergan@hotmail.com>
2016-04-04 21:17:40 +1000

    Update rom4.v
    Fixed 'in'

--- src/rom4.v
+++ src/rom4.v
@@ -1,6 +1,6 @@
 module rom4 (input wire [3:0] addr, output reg [7:0] data);
-    always @(in) begin
-        case(in)
+    always @(addr) begin
+        case(addr)
             4'b0000: data = 8'b01000110; // F
             4'b0001: data = 8'b01010000; // P
             4'b0010: data = 8'b01000111; // G
